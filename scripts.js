cced52d1a554a032c7ad7b997ff5e6c6e361b64d
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-11-25 21:08:09 -0300

    ADD função sticky nav menu

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
 "use strict";
 
+///////////////////
+// Menu Sticky - Fixed
+
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+
+const stickyNav = function(entries) {
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) nav.classList.add('fixed');
+    else nav.classList.remove('fixed');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+});
+
+headerObserver.observe(header);
+
+////////////////////////////////////////
 // Exibir conteúdo ao rolar página
 
 const sections = document.querySelectorAll(".section");
